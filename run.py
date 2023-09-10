35a619906fd10c07be51ee5fbc1940fb27b88853
devjldp <joseluisdp297@gmail.com>
2023-09-10 11:06:49 +0100

    Retrieve latest stock numbers from  spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -24,8 +26,10 @@ print(data)
 
 def get_sales_data():
     """
-    Get sales figures from the user
-    
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by comma. The loop repeatedly request data, until is valid.    
     """
     while True:
         print("Please enter sales data form alst market")
@@ -61,7 +65,36 @@ def validate_data(values):
     
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, and new row with the list data provided.
+    """
+    print("Updating sales worksheet... \n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated succesfully.\n")
+
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+
+    The surplus is defined as the sales figure substracted from the stock:
+    - Positive surplus indicates waste.
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet(stock).get_all_values()
+    stock_row = stock[-1]
 
 
-data = get_sales_data()
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+# transform element in list into integer
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
 
+print("Welcome to Love Sandwiches Data Automation")
+main()
